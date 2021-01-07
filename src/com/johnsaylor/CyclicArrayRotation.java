7ba2463cc6c6da55c78e580b4bf331be2d1ec077
jmsaylor <johnmsaylor@gmail.com>
2021-01-06 20:43:04 -0500

    improvements and variation

--- src/com/johnsaylor/CyclicArrayRotation.java
+++ src/com/johnsaylor/CyclicArrayRotation.java
@@ -5,11 +5,30 @@ import java.util.Arrays;
 public class CyclicArrayRotation {
 
     public static void test() {
-        System.out.println(Arrays.toString(solution(new int[]{1,2,3}, 100)));
+        System.out.println(Arrays.toString(solution(new int[]{1,2,3}, 2)));
 
     }
 
-    public static int[] solution(int[] A, int K) {
+    public static int[] solution(int[] A, int K){
+        int[] result = new int[A.length];
+        for (int i = 0; i < result.length; i++) {
+            result[((i + K) % A.length)] = A[i];
+        }
+        return result;
+    }
+
+    public static int[] improvements(int[] A, int K) {
+
+        int[] result = new int[A.length];
+
+        for (int i = 0; i < A.length; i++) {
+            result[i] = A[((i + K) % A.length)];
+        }
+
+        return result;
+    }
+
+    public static int[] submission(int[] A, int K) {
 
         int[] result = new int[A.length];
         int marker = K >= A.length ? K - ((K / A.length) * A.length) : K - 1;
