8d7a242fc678fafaad4e70d284ddac358871a918
jmsaylor <johnmsaylor@gmail.com>
2021-01-10 18:12:20 -0500

    fish algo w/ stack

---
+++ src/com/johnsaylor/Fish.java
@@ -0,0 +1,30 @@
+package com.johnsaylor;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+
+public class Fish {
+
+    public static void test() {
+        solution(new int[]{4, 3, 2, 1, 5}, new int[]{0, 1, 0, 0, 0});
+    }
+
+    public static int solution(int[] A, int[] B) {
+        // write your code in Java SE 8
+        Deque<Integer> stack = new ArrayDeque<>();
+        int survivors = 0;
+
+        for (int i = 0; i < B.length; i++) {
+            if (B[i] == 1){
+                stack.addLast(A[i]);
+            } else {
+                while (!stack.isEmpty() && stack.peekLast() < A[i]) stack.removeLast();
+                if (stack.isEmpty()) survivors++;
+            }
+            System.out.println(stack + "   " + survivors);
+        }
+        return survivors + stack.size();
+
+    }
+}
