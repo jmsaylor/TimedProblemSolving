e9ac638f466289a1dfdf47fd39a1d2b3a9f36afc
jmsaylor <johnmsaylor@gmail.com>
2021-01-12 21:45:57 -0500

    Kadanes Algo / Max Sub Array

---
+++ src/com/johnsaylor/MaxSubArray.java
@@ -0,0 +1,23 @@
+package com.johnsaylor;
+
+public class MaxSubArray {
+
+    public static void test() {
+        System.out.println(solution(new int[]{100,110,120, 100, 110, 130}));
+    }
+
+    public static int solution(int[] A){
+        //Kadane's algo
+        int localMax = 0;
+        int globalMax = 0;
+
+        for (int i = 1 ; i < A.length; i++) {
+            int delta = A[i] - A[i -1];
+            localMax = Math.max(delta, localMax + delta);
+            globalMax = Math.max(localMax, globalMax);
+        }
+
+        return globalMax;
+
+    }
+}
