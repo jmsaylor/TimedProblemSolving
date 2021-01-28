9279d1d095c1718a58058697f9ba264b706d8aaf
jmsaylor <johnmsaylor@gmail.com>
2021-01-28 11:25:44 -0500

    linked list reversal

--- src/com/johnsaylor/Main.java
+++ src/com/johnsaylor/Main.java
@@ -1,9 +1,12 @@
 package com.johnsaylor;
 
+import com.johnsaylor.codesignal.LinkedListReverse;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        MaxSubArray.test();
+        var test = new LinkedListReverse();
+        test.test();
     }
 }
--- src/com/johnsaylor/codesignal/LinkedListReverse.java
+++ src/com/johnsaylor/codesignal/LinkedListReverse.java
@@ -14,16 +14,68 @@ public class LinkedListReverse {
     public void test() {
         Node<Integer> head = new Node<>(1);
         head.next = new Node<>(2);
-        head.next.next = new Node<>(1);
-        isListPalindrome(head);
+        head.next.next = new Node<>(3);
+        head.next.next.next = new Node<>(3);
+        head.next.next.next.next = new Node<>(4);
+        head.next.next.next.next.next = new Node<>(1);
+
+        System.out.println(isListPalindrome(head));
     }
 
     boolean isListPalindrome(Node<Integer> l) {
-        Node<Integer> prev = null;
-        Node<Integer> curr = l;
-        Node<Integer> next = l;
+        Node<Integer> first = l;
+        Node<Integer> second = reverse(findHalf(l));
 
+        while (first.next != null && second.next != null) {
+            if (first.value != second.value) return false;
+            first = first.next;
+            second = second.next;
+        }
 
         return true;
     }
+
+    public Node<Integer> findHalf(Node<Integer> head){
+        Node<Integer> slow = head;
+        Node<Integer> fast = head;
+
+        while (fast.next != null) {
+            if (fast.next.next != null) {
+                // fast jumps two, slow one
+                fast = fast.next.next;
+                slow = slow.next;
+            } else {
+                // fast jumps one, slow stop
+                fast = fast.next;
+            }
+//            System.out.println(slow.value + " " + fast.value);
+        }
+
+        Node<Integer> halfHead = slow.next;
+        slow.next = null;
+
+        return halfHead;
+    }
+
+    public Node<Integer> reverse(Node<Integer> l) {
+        Node<Integer> previous = null;
+        Node<Integer> current = l;
+        Node<Integer> following = l;
+
+        while (current != null) {
+            following = following.next;
+            current.next = previous;
+            previous = current;
+            current = following;
+        }
+
+        return previous;
+    }
+
+    public void print(Node<Integer> head) {
+        if (head == null) return;
+        System.out.print(head.value);
+        print(head.next);
+    }
 }
+
