d256c77ecec09aa4c2e451279667f9744f506043
jmsaylor <johnmsaylor@gmail.com>
2021-01-30 22:06:03 -0500

    Linked List reversal for comparison / palindrome

--- src/com/johnsaylor/codesignal/LinkedListReverse.java
+++ src/com/johnsaylor/codesignal/LinkedListReverse.java
@@ -12,21 +12,68 @@ class Node<T> {
 public class LinkedListReverse {
 
     public void test() {
-        Node<Integer> head = new Node<>(1);
-        head.next = new Node<>(2);
+        Node<Integer> head = new Node<>(3);
+        head.next = new Node<>(5);
         head.next.next = new Node<>(3);
-        head.next.next.next = new Node<>(3);
-        head.next.next.next.next = new Node<>(4);
-        head.next.next.next.next.next = new Node<>(1);
+        head.next.next.next = new Node<>(5);
+        head.next.next.next.next = new Node<>(1000000000);
+//        head.next.next.next.next.next = new Node<>(1);
 
-        System.out.println(isListPalindrome(head));
+        System.out.println(isListPalindromeCS(head));
+    }
+
+    boolean isListPalindromeCS(Node<Integer> l) {
+        if (l == null) return true;
+        Node<Integer> fast = l;
+        Node<Integer> slow = l;
+
+        Node<Integer> prev = null;
+        Node<Integer> curr = slow;
+        Node<Integer> foll = slow;
+
+        boolean isEven = false;
+
+        while(fast.next != null) {
+//            System.out.println(fast.value);
+            if (fast.next.next != null) {
+                fast = fast.next.next;
+            } else {
+                isEven = true;
+                fast = fast.next;
+            }
+
+            slow = slow.next;
+            //reverse
+            foll = foll.next;
+            curr.next = prev;
+            prev = curr;
+            curr = foll;
+        }
+
+        Node<Integer> second = isEven ? slow : slow.next;
+        Node<Integer> first = prev;
+
+        print(first);
+        System.out.println();
+        print(second);
+        System.out.println();
+
+        while (first != null && second != null) {
+            System.out.println(first.value + " " + second.value);
+            if (!first.value.equals(second.value)) return false;
+            first = first.next;
+            second = second.next;
+        }
+
+        return true;
     }
 
     boolean isListPalindrome(Node<Integer> l) {
         Node<Integer> first = l;
         Node<Integer> second = reverse(findHalf(l));
 
-        while (first.next != null && second.next != null) {
+        while (first != null && second != null) {
+            System.out.println(first.value + " - " + second.value);
             if (first.value != second.value) return false;
             first = first.next;
             second = second.next;
@@ -74,8 +121,9 @@ public class LinkedListReverse {
 
     public void print(Node<Integer> head) {
         if (head == null) return;
-        System.out.print(head.value);
+        System.out.print(head.value + "  ");
         print(head.next);
     }
+
 }
 
