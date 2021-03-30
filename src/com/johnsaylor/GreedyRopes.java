c60c6df46af82edd004b5bf8f186699033286517
jmsaylor <johnmsaylor@gmail.com>
2021-03-30 19:29:27 -0400

    greedy algorithm

---
+++ src/com/johnsaylor/GreedyRopes.java
@@ -0,0 +1,19 @@
+package com.johnsaylor;
+
+public class GreedyRopes {
+    public int solution(int K, int[] A) {
+        // write your code in Java SE 8
+
+        int ropes = 0;
+        int currentLength = 0;
+
+        for (int rope : A){
+            currentLength+=rope;
+            if (currentLength >= K) {
+                ropes++;
+                currentLength = 0;
+            }
+        }
+        return ropes;
+    }
+}
