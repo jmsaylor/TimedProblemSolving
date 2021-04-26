d02a676fe846fd69552d3f735a8b55b23bbba867
John <johnmsaylor@gmail.com>
2021-04-26 18:11:31 -0400

    longest palindrome - dynamic programming

--- src/com/johnsaylor/leet/LongestPalindromicSubstring.java
+++ src/com/johnsaylor/leet/LongestPalindromicSubstring.java
@@ -1,12 +1,52 @@
 package com.johnsaylor.leet;
 
+import java.util.Arrays;
+
 public class LongestPalindromicSubstring {
 
     public static void main(String[] args) {
-        System.out.println(longestPalindrome("aacabdkacaa"));
+        System.out.println(longestPalindrome("aaaabbaa"));
     }
 
     public static String longestPalindrome(String s) {
+        char[] chars = s.toCharArray();
+        boolean[][] table = new boolean[s.length()][s.length()];
+        int[] maxPalindrome = {0, 0};
+
+
+        for (int i = 0; i < table.length; i++) {
+            table[i][i] = true;
+        }
+
+        for (int i = 0; i < table.length - 1; i++) {
+            if (chars[i] == chars[i + 1]) {
+                table[i][i + 1] = true;
+                maxPalindrome[0] = i;
+                maxPalindrome[1] = i + 1;
+            }
+        }
+
+        for (int length = 3; length <= table.length; length++) {
+            int distance = length - 1;
+            for (int i = 0; i + distance < table.length; i++) {
+                if(chars[i] == chars[i + distance] && table[i + 1][i + distance - 1]) {
+                      table[i][i + distance] = true;
+                      if ((i + distance) - i > maxPalindrome[1] - maxPalindrome[0]) {
+                          maxPalindrome[0] = i;
+                          maxPalindrome[1] = i + distance;
+                      }
+                }
+            }
+        }
+
+        for (boolean[] row : table) {
+            System.out.println(Arrays.toString(row));
+        }
+
+        return s.substring(maxPalindrome[0], maxPalindrome[1] + 1);
+    }
+
+    public static String longestPalindrome2(String s) {
         if (s.length() == 0) return "";
         char[] chars = s.toCharArray();
         String maxSubstring = s.substring(0,1);
