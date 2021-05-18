f6d1d743fd0247e05f801a9e5405639530564695
John <johnmsaylor@gmail.com>
2021-05-17 20:35:39 -0400

    regex solution - slower than expected

--- src/com/johnsaylor/leet/PalindromeNumber.java
+++ src/com/johnsaylor/leet/PalindromeNumber.java
@@ -1,8 +1,29 @@
 package com.johnsaylor.leet;
 
+import java.util.regex.Pattern;
+
 public class PalindromeNumber {
 
+    public static void main(String[] args) {
+        System.out.println( isPalindrome(18081));
+    }
+
     //TODO: bitwise solution
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        if (x < 10) return true;
+
+        String string = String.valueOf(x);
+        StringBuilder firstHalf  = new StringBuilder(string.substring(0, string.length() / 2));
+
+        Pattern pattern = Pattern.compile(firstHalf.reverse().toString());
+
+        int startSecondHalf = string.length() % 2 == 0 ? string.length() / 2 : string.length() / 2 + 1;
+
+        return pattern.matcher(string.substring(startSecondHalf)).matches();
+
+    }
 
     public boolean isPalindrome1(int x) {
         if (x < 0) return false;
